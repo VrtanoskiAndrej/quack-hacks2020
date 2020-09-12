6c8b47813574155249d2f5a26f2c1e5c1da7baa4
Andrej <31016718+VrtanoskiAndrej@users.noreply.github.com>
2020-09-12 09:08:48 +0400

    Test Push

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ class App extends Component {
         return (
             <div className="App">
                <h1>Hello World</h1>
+                <h2>This is a test</h2>
             </div>
         );
     }
